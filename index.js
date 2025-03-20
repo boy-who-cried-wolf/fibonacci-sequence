6b66e5de5bd565ae337cadd45f34566b7e212ad7
boy-who-cried-wolf <ckang215@gmail.com>
2025-03-20 13:55:11 +0800

    feat: upgrade performace

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const evolve = (n, f = x => (x > 1 ? f (x -1) + f (x -2 ) : x )) => f (n);
+const evolve = (n, f = x => (x > 1 ? f(x-1) + f(x-2) : x)) => f(n);
+
+// The Fibonacci sequence modulo 9 repeats every 24 numbers
+const PATTERN_LENGTH = 24;
 
 const generateMatrix = (size) => Array.from({length: size}, (_, i) => 
-Array.from({length: size}, (_, j) => evolve(i + j) % 9));
+    Array.from({length: size}, (_, j) => {
+        // Calculate the position in the pattern
+        const pos = (i + j) % PATTERN_LENGTH;
+        // Use evolve for the actual calculation
+        return evolve(pos) % 9;
+    }));
 
-console.log(generateMatrix(6));
+// Test with a larger matrix
+console.log(generateMatrix(200));
